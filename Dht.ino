e2e370ee0bfd614b879e900b36e104744d12510a
leedongyeop <20181676@edu.hanbat.ac.kr>
2022-05-09 21:54:11 +0900

    Dht.ino upload

---
+++ Dht.ino
@@ -0,0 +1,30 @@
+#include "DHT.h"
+
+#define DHTPIN 2     // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("DHTxx test!"));
+
+  dht.begin();
+}
+
+void loop() {
+  delay(2000);
+
+  float humidity = dht.readHumidity(); //습도 주세요(실수 형태로)
+  float temperature = dht.readTemperature(); //온도 주세요 실수 값으로
+
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.print("%  Temperature: ");
+  Serial.print(temperature);
+  Serial.println("°C ");
+}
